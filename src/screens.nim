9845ea784dfbbad7388849ccf3280ec7568e5547
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-06-25 17:14:31 +0200

    Add background effect

--- src/screens.nim
+++ src/screens.nim
@@ -3,18 +3,28 @@ import
   strutils,
   unicode,
   random,
+  times,
+  math,
+
   nimraylib_now,
+  
   consts,
-  screen,
-  utils
+  screen
 
 type
   GameScreen* = ref object of Screen
     words: seq[string]
+    
     word: string
     input: string
+
     total: int
-    success: int
+    bad: int
+
+    overlayColor: Color
+    overlayAlpha: float
+
+    endTime: float
 
 proc newGameScreen*(): GameScreen = new result
 
@@ -26,13 +36,23 @@ method init*(s: GameScreen) =
   randomize()
   s.words = toSeq(lines "assets/words.txt")
   s.reset()
+  s.endTime = times.getTime().toUnixFloat()
 
 method update*(s: GameScreen) = 
   beginDrawing:
     clearBackground(White)
 
     block:
+      let alpha = uint8(255 * s.overlayAlpha)
+
+      s.overlayColor.a = alpha
+      s.overlayAlpha = lerp(s.overlayAlpha, 0f, 1f - pow(0.33f, getFrameTime()))
+
+      drawRectangle(0, 0, Size, Size, s.overlayColor)
+
+    block:
       let width = measureText(s.word, 40) div 2 
+
       drawText(s.word, Center - width, Center - 20, 40, Lightgray)
       drawText(s.input, Center - width, Center - 20, 40, Black)
 
@@ -48,8 +68,12 @@ method update*(s: GameScreen) =
 
     block:
       if not s.word.startsWith(s.input):
+        s.overlayColor = Red
+        s.overlayAlpha = 1f
+        s.bad.inc()
         s.reset()
 
       if s.word == s.input:
-        s.success.inc
+        s.overlayColor = Green
+        s.overlayAlpha = 1f
         s.reset()
